3b57de81165e68ca482e6e4ffa2b81b60bf0e758
Roman <didoshakr@ukr.net>
2025-07-18 19:09:28 +0300

    d

--- app/components/header/SiteFooter.jsx
+++ app/components/header/SiteFooter.jsx
@@ -38,6 +38,27 @@ const SiteFooter = () => {
           <ul className="flex space-x-4 items-center">
             <li>
               <a
+                className="m-1 space-x-2 flex list-none flex-nowrap  items-center text-base font-normal text-hText  hover:bg-hBgHov  hover:text-hTextHov dark:text-hTextD dark:hover:bg-hBgHovD dark:hover:text-hTextHovD"
+                href="https://github.com/didoshakra?tab=repositories"
+                title="GitHub"
+              >
+                <svg
+                  className="h-6 w-6  dark:hover:text-hTextHovD"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  {" "}
+                  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
+                </svg>
+                {/* <p>GitHub</p> */}
+              </a>
+            </li>
+            <li>
+              <a
                 href="https://www.facebook.com/profile.php?id=100017742340573"
                 target="_blank"
                 rel="noopener noreferrer"
