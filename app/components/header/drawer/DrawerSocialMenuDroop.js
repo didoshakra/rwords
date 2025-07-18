0609054609a4e056e495c3e9a8f04dcc06841b3c
Roman <didoshakr@ukr.net>
2025-07-18 20:00:56 +0300

    e

--- app/components/header/drawer/DrawerSocialMenuDroop.js
+++ app/components/header/drawer/DrawerSocialMenuDroop.js
@@ -4,7 +4,7 @@
 "use client"
 import { useState } from "react"
 
-const DrawerSocialMenuDroop = ({ setDrawerOpen }) => {
+const DrawerSocialMenuDroop = () => {
   const [drawerSocialMenuDroop, setDrawerSocialMenuDroop] = useState(false)
 
   //випадаюче меню Налаштувань
