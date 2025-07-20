56e59ac824743f0ce699e2d0241bde73dba20c43
Roman <didoshakr@ukr.net>
2025-07-20 14:34:26 +0300

    2

--- app/components/header/HeaderLogo.js
+++ app/components/header/HeaderLogo.js
@@ -11,8 +11,8 @@ const HeaderLogo = () => {
         <div className=" relative items-center h-10 w-16">
           <Image
             src="/images/home/sun_man_hands-oval-ra-red-64-41.png"
-              height={41} //Завжди Avto?
-              width={64} //Чомусь має пріорітет
+            height={41} //Завжди Avto?
+            width={64} //Чомусь має пріорітет
             alt="Logo"
             priority
           />
@@ -21,6 +21,7 @@ const HeaderLogo = () => {
       <Link href="/">
         <div className="flex items-center justify-start px-1 text-xl font-bold italic text-hText hover:text-hTextHov hover:bg-hBgHov dark:text-hTextD dark:hover:text-hTextHovD dark:hover:bg-hBgHovD md:px-2 md:text-2xl">
           RWords
+          <span style={{ fontSize: "16px", fontWeight: "bold", marginLeft: "8px" }}>(В розробці)</span>
         </div>
       </Link>
       {/* ========================================================================== */}
