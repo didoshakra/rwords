55d71428c0f1166e463a12cf41d740ed1fbd23c7
Roman <didoshakr@ukr.net>
2025-08-12 14:34:34 +0300

    к

--- app/components/header/HeaderMenuRight.js
+++ app/components/header/HeaderMenuRight.js
@@ -1,46 +1,28 @@
-//HeaderMenuRight.js  /Верхнє меню праве.
-//щоб він не вмикав “десктопне” меню, коли мобільний телефон у горизонтальному режимі.
+//HeaderMenuRight.js  /Верхнє меню
 "use client"
 
-import { useState, useEffect } from "react"
+// import ThemeSwitcher from "./ThemeSwitcher"
 import UserSwitcher from "./user/UserSwitcher.js"
 import HeaderSetingDroopMenu from "./HeaderMobileDroopMenu.js"
 import HeaderThemesDroopMenu from "./theme/HeaderThemesMenuButton.js"
 
 const HeaderMenuRight = () => {
-  const [showDesktopMenu, setShowDesktopMenu] = useState(false)
-
-useEffect(() => {
-  function checkScreen() {
-    const isMobileAgent = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent)
-    const isWideScreen = window.innerWidth >= 768
-    const show = !isMobileAgent && isWideScreen
-    console.log("isMobileAgent:", isMobileAgent, "window.innerWidth:", window.innerWidth, "showDesktopMenu:", show)
-    setShowDesktopMenu(show)
-  }
-
-  checkScreen()
-  window.addEventListener("resize", checkScreen)
-  return () => window.removeEventListener("resize", checkScreen)
-}, [])
-
   return (
-    <div className="flex items-center justify-end h-full">
-      {showDesktopMenu && (
-        <div className="flex h-full space-x-2">
-          <UserSwitcher />
-          <HeaderThemesDroopMenu />
-        </div>
-      )}
-      {!showDesktopMenu && (
-        <div className="headerMenuRight__mobile" style={{ display: "none" }}>
-          <HeaderSetingDroopMenu />
-        </div>
-      )}
+    // <div className=" flex h-16 items-center justify-between">
+    <div className=" flex items-center justify-end  h-full">
+      {/* Іконки головного меню Seting */}
+      <div className="hidden md:flex  h-full">
+        {/* User */}
+        <UserSwitcher />
+        {/*Всі теми */}
+        <HeaderThemesDroopMenu />
+      </div>
+      {/* Випадаюче меню Seting(мобільне) */}
+      <div className="headerMenuRight__mobile">
+        <HeaderSetingDroopMenu />
+      </div>
     </div>
   )
 }
 
 export default HeaderMenuRight
-
-
