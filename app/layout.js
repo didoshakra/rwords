7a55e646d6edc882609422efca5259e0b755edce
Roman <didoshakr@ukr.net>
2026-01-24 13:23:35 +0200

    йй

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,30 @@
 // app/layout.jsx
 //Так як layout є серверним компонентом — він отримає статистику і передасть її в HeaderTape і не треба створювати обгортку HeaderTapeWrapper.js
+/* =================== */
+/* 2️⃣ Підключення тем */
+/* =================== */
+/* ВСЕ @import має бути тут, поза @layer */
+import "@/styles/themes/tIndigo.css";
+import "@/styles/themes/tPink.css";
+import "@/styles/themes/tGreen.css";
+import "@/styles/themes/tAmber.css";
+import "@/styles/themes/tGray.css";
+import "@/styles/themes/tLime.css";
+import "@/styles/themes/tRed.css";
+import "@/styles/themes/tYellow.css";
+import "@/styles/themes/tEmerald.css";
+import "@/styles/themes/tTeal.css";
+import "@/styles/themes/tCyan.css";
+import "@/styles/themes/tSky.css";
+import "@/styles/themes/tBlue.css";
+import "@/styles/themes/tViolet.css";
+import "@/styles/themes/tPurple.css";
+import "@/styles/themes/tFuchsia.css";
+import "@/styles/themes/tRose.css";
+import "@/styles/themes/tOrange.css";
+import "@/styles/themes/tZinc.css";
+import "@/styles/themes/tNeutral.css";
+import "@/styles/themes/tStone.css";
 import "./globals.css"
 import { Inter } from "next/font/google"
 import { headers } from "next/headers"
