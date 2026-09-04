601101dcaf27cd622dca8d9ba305c9e989693707
Roman <didoshakr@ukr.net>
2026-09-04 18:20:44 +0300

    rrrrrr

--- app/actions/cloudinary/addPicture.js
+++ app/actions/cloudinary/addPicture.js
@@ -9,9 +9,10 @@ export async function addPicture(filePath) {
     // Завантаження в Cloudinary
     const res = await cloudinary.uploader.upload(filePath, {
       folder: "pictures",
+      resource_type: "auto", // авто-визначення: image / video / raw
     })
 
-    // res містить: secure_url, format, width, height, bytes, public_id і т.д.
+    // resvv містить: secure_url, format, width, height, bytes, public_id і т.д.
     const dbRes = await sql`
       INSERT INTO pictures (url, format, width, height, bytes, public_id)
       VALUES (${res.secure_url}, ${res.format}, ${res.width}, ${res.height}, ${res.bytes}, ${res.public_id})
